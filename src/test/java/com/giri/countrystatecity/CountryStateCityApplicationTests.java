df63123f8e23fde970ab99dfa23709309b88842a
Giridhar Pottepalem <giri.pottepalem@hmhco.com>
2024-09-13 13:31:22 -0400

    Updated Application smokeTest

--- src/test/java/com/giri/countrystatecity/CountryStateCityApplicationTests.java
+++ src/test/java/com/giri/countrystatecity/CountryStateCityApplicationTests.java
@@ -1,13 +1,22 @@
 package com.giri.countrystatecity;
 
+import com.giri.countrystatecity.service.CountryStateCityService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    useMainMethod = SpringBootTest.UseMainMethod.ALWAYS // Get the main method coverage by this from this smoke test
+)
 class CountryStateCityApplicationTests {
+    @Autowired
+    CountryStateCityService countryStateCityService;
 
     @Test
-    void contextLoads() {
+    void smokeTest_contextLoads() {
+        assertThat(countryStateCityService).isNotNull();
     }
-
 }
